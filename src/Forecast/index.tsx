8c80573b81300e3dc8b36677803b92ded6480af3
Ben Winters <ben@blwinters.com>
2022-03-13 12:00:49 -0400

    Clean up CRA defaults

--- src/Forecast/index.tsx
+++ src/Forecast/index.tsx
@@ -1,1 +1,8 @@
 import React from 'react'
+import { Button } from '@mui/material'
+
+const Forecast = () => {
+  return <Button variant="contained">Tap Me</Button>
+}
+
+export default Forecast
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import Forecast from './Forecast'
 import reportWebVitals from './reportWebVitals'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Forecast />
   </React.StrictMode>,
   document.getElementById('root')
 )
