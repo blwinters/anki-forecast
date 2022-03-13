00c1c374a727a17cde991c18819841138c826cc2
Ben Winters <ben@blwinters.com>
2022-03-13 13:06:38 -0400

    Connect button to forecast generation

--- src/Forecast/index.tsx
+++ src/Forecast/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Button } from '@mui/material'
 import Chart from './ui/Chart'
 import { DayLearningSummary } from './logic/generateForecast/types'
@@ -10,17 +10,24 @@ import {
 } from './logic/generateForecast/forecastHelpers'
 
 const Forecast = () => {
-  const data: DayLearningSummary[] = generateForecast({
-    startingSummary: defaultStartingSummary({ deckSize: 2000 }),
-    forecastLength: 60,
-    ankiConfig: defaultAnkiConfig,
-    weekConfig: defaultWeekConfig,
-  })
+  const [data, setData] = useState<DayLearningSummary[]>([])
+
+  const onUpdate = useCallback(() => {
+    const forecast = generateForecast({
+      startingSummary: defaultStartingSummary({ deckSize: 2000 }),
+      forecastLength: 60,
+      ankiConfig: defaultAnkiConfig,
+      weekConfig: defaultWeekConfig,
+    })
+    setData(forecast)
+  }, [])
 
   return (
     <>
       <Chart data={data} />
-      <Button variant="contained">Update</Button>
+      <Button variant="contained" onClick={onUpdate}>
+        Update
+      </Button>
     </>
   )
 }
