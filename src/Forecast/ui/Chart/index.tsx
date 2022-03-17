1661f47e47fb7a1b29ec973520fcf27b4c70ed41
Ben Winters <ben@blwinters.com>
2022-03-17 00:03:17 -0400

    Add theme and additional properties to chart

--- src/Forecast/ui/Chart/index.tsx
+++ src/Forecast/ui/Chart/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts'
+import { Area, ComposedChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 import { DaySummary } from '../../logic/generateForecast/types'
 
 export interface ChartProps {
@@ -10,9 +10,9 @@ const Chart = ({ data }: ChartProps) => {
   const chartData = mapDataForChart(data)
 
   return (
-    <BarChart
-      width={800}
-      height={500}
+    <ComposedChart
+      width={1600}
+      height={700}
       data={chartData}
       margin={{
         top: 20,
@@ -20,18 +20,66 @@ const Chart = ({ data }: ChartProps) => {
         left: 20,
         bottom: 5,
       }}>
+      <defs>
+        <linearGradient id="colorTotal" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor={theme.learnedArea} stopOpacity={0.8} />
+          <stop offset="95%" stopColor={theme.learnedArea} stopOpacity={0} />
+        </linearGradient>
+      </defs>
       <XAxis name="Day" dataKey="name" />
-      <YAxis />
+      <YAxis
+        yAxisId="left"
+        type="number"
+        dataKey="totalReviews"
+        name="Daily Reviews"
+        unit=""
+        orientation="left"
+        stroke={theme.totalReviews}
+      />
+      <YAxis
+        yAxisId="right"
+        type="number"
+        dataKey="totalActive"
+        name="Total Learned"
+        unit=""
+        orientation="right"
+        stroke={theme.totalLearned}
+      />
       <Tooltip />
       <Legend />
-      <Bar dataKey="totalActive" name="Total Reviews" stackId="a" fill="#8884d8" />
-    </BarChart>
+      <Area
+        yAxisId="right"
+        type="monotone"
+        dataKey="totalActive"
+        name="Total Learned"
+        stroke={theme.learnedArea}
+        fillOpacity={1}
+        fill="url(#colorTotal)"
+      />
+      <Bar yAxisId="left" dataKey="newReviews" name="New Cards" stackId="a" fill={theme.new} />
+      <Bar
+        yAxisId="left"
+        dataKey="youngReviews"
+        name="Young Reviews"
+        stackId="a"
+        fill={theme.young}
+      />
+      <Bar
+        yAxisId="left"
+        dataKey="matureReviews"
+        name="Mature Reviews"
+        stackId="a"
+        fill={theme.mature}
+      />
+    </ComposedChart>
   )
 }
 
 interface DataPoint {
   name: string
   newReviews: number
+  youngReviews: number
+  matureReviews: number
   totalReviews: number
   totalActive: number
 }
@@ -41,10 +89,32 @@ const mapDataForChart = (data: DaySummary[]): DataPoint[] => {
     return {
       name: `${index + 1}`,
       newReviews: summary.reviews.new,
+      youngReviews: summary.reviews.young,
+      matureReviews: summary.reviews.mature,
       totalReviews: summary.reviews.total,
       totalActive: summary.endCounts.totalActive,
     }
   })
 }
+
+interface ForecastTheme {
+  new: string
+  young: string
+  mature: string
+  learnedArea: string
+  totalLearned: string
+  totalReviews: string
+}
+
+const themeA: ForecastTheme = {
+  new: '#fc9967',
+  young: '#a4de6c',
+  mature: '#83a6ed',
+  learnedArea: '#8884d8',
+  totalLearned: '#888888',
+  totalReviews: '#888888',
+}
+
+const theme: ForecastTheme = themeA
 
 export default Chart
