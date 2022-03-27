bfd01646c23503c8a245a85a6c504619b7668806
Ben Winters <ben@blwinters.com>
2022-03-26 20:15:02 -0400

    Add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,8 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:prettier/recommended',
   ],
   rules: {
