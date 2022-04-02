2bcab225e92820a2bbfc2947c548cd2332536732
Ben Winters <ben@blwinters.com>
2022-04-02 16:00:39 -0400

    Revise welcome text

--- src/Welcome/index.tsx
+++ src/Welcome/index.tsx
@@ -14,10 +14,11 @@ export const Welcome = ({ onProceed }: Props) => (
           </Typography>
         </StackItem>
         <StackItem>
-          <Typography variant="body1" sx={{ maxWidth: 650, textAlign: 'center' }}>
-            Anki Forecast is a tool for calculating your flash card progress and review counts over
-            a period of time. You can use the basic parameters or change them by day of the week.
-            Several of the Anki deck options are also configurable to tune your forecast.
+          <Typography variant="body1" sx={{ maxWidth: 680, textAlign: 'center' }}>
+            Anki Forecast is a tool for calculating your future flash card reviews and total
+            progress. You can use the basic settings, change them by day of the week, or skip a
+            certain number of days per month. Several of the Anki deck options are also available
+            for tuning your forecast.
           </Typography>
         </StackItem>
         <StackItem>
